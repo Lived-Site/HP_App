09a6e5bb0d7f6724c3d7acadf20adf7f9ce430df
Francisco Menchaca Giorello <franmenchaca2004@gmail.com>
2026-07-17 16:36:09 -0300

    Feature: Preguntas y cuestionarios

---
+++ Domain/Cuestionario.cs
@@ -0,0 +1,45 @@
+﻿namespace Domain;
+
+public class Cuestionario
+{
+    private static int ultimoId = 0;
+    public int IdCuestionario { get; set; }
+    public int Dificultad { get; set; }
+    public List<Pregunta> Preguntas { get; set; }
+
+    public Cuestionario(List<Pregunta> preguntas)
+    {
+        if (preguntas == null)
+            throw new ArgumentNullException(nameof(preguntas));
+
+        if (preguntas.Count == 0)
+            throw new ArgumentException("El cuestionario debe tener al menos una pregunta.");
+
+        IdCuestionario = ultimoId;
+        ultimoId++;
+
+        Preguntas = preguntas;
+        Dificultad = AsignarDificultad();
+    }
+
+    public void AgregarPregunta(Pregunta pregunta)
+    {
+        if (pregunta == null)
+            throw new ArgumentNullException(nameof(pregunta));
+
+        Preguntas.Add(pregunta);
+        Dificultad = AsignarDificultad();
+    }
+
+    private int AsignarDificultad()
+    {
+        int suma = 0;
+
+        foreach (Pregunta pregunta in Preguntas)
+        {
+            suma += pregunta.Dificultad;
+        }
+
+        return suma / Preguntas.Count;
+    }
+}
---
+++ Domain/Pregunta.cs
@@ -0,0 +1,64 @@
+﻿namespace Domain;
+
+public class Pregunta
+{
+    private static int UltimoId = 0;
+    public int IdPregunta { get; set; }
+    public string Letra { get; set; }
+    public int Dificultad { get; set; }
+    public List<string> Respuestas { get; set; }
+    public string RespuestaCorrecta { get; set; }
+
+    public Pregunta(string letra, int dificultad, List<string> respuestas, string respuestaCorrecta)
+    {
+        this.IdPregunta = UltimoId;
+        UltimoId++;
+        if (string.IsNullOrWhiteSpace(Letra)) 
+            throw new ArgumentException("Debe ingresar una letra");
+        if (string.IsNullOrWhiteSpace(RespuestaCorrecta))
+            throw new ArgumentException("Debe ingresar una respuesta correcta");
+        if(Respuestas.Count < 2)
+            throw new ArgumentException("Debe ingresar al menos 2 respuesta");
+        if (dificultad < 1)
+            throw new ArgumentException("La dificultad debe ser mayor que 0");
+        if (!respuestas.Contains(respuestaCorrecta))
+            throw new ArgumentException("La respuesta correcta debe estar entre las opciones.");
+        
+        this.Letra = Letra;
+        this.Dificultad = Dificultad;
+        this.Respuestas = respuestas;
+        this.RespuestaCorrecta = RespuestaCorrecta;
+    }
+
+    public void ModificarPregunta(string nuevaPregunta)
+    {
+        if (string.IsNullOrWhiteSpace(nuevaPregunta))
+            throw new ArgumentException("Debe ingresar una nueva pregunta");
+        Letra = nuevaPregunta;
+    }
+
+    public void ModificarRespuestaCorrecta(string nuevaRespuestaCorrecta)
+    {
+        if (string.IsNullOrWhiteSpace(nuevaRespuestaCorrecta))
+            throw new ArgumentException("Debe ingresar una respuesta correcta");
+        RespuestaCorrecta = nuevaRespuestaCorrecta;
+    }
+
+    public void ModificarOpcionesDeRespuestas(List<string> nuevasRespuestas)
+    {
+        if (nuevasRespuestas.Count < 2)
+            throw new ArgumentException("Debe ingresar al menos 2 opciones");
+
+        Respuestas = nuevasRespuestas;
+    }
+
+    public void ModificarDificultad(int nuevaDificultad)
+    {
+        if(nuevaDificultad < 1)
+            throw new ArgumentException("La dificultad debe ser mayor que cero");
+        Dificultad = nuevaDificultad;
+    }
+    
+
+    
+}
